975b1d6198233dd6e4cf6fa35fcd778fd972327d
root <root@DESKTOP-R9UQAHO>
2023-12-26 14:39:42 +0900

    laravel表示

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/', [TestController::class, 'index']);
 /*
 |--------------------------------------------------------------------------
